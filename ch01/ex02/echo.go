3210a804eeb0994162dbc8caefe2aef6b9099522
kurohune538 <kurohune538@gmail.com>
2018-02-16 09:17:03 +0900

    ex01,02

---
+++ ch01/ex02/echo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep, result string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+		result = fmt.Sprintf("%d : %s", i, os.Args[i])
+		fmt.Println(result)
+	}	
+}
